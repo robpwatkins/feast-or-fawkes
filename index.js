38af782d6897e1d36990bf91860e58b41b8436c7
robpwatkins <robpwatkins@gmail.com>
2021-11-14 15:00:25 -0600

    figuring out how to pass checkLetter to setWord

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const wordContainer = document.querySelector('#word-container');
 window.addEventListener('keydown', startGame);
 
 function startGame() {
+  window.removeEventListener('keydown', startGame);
   title.classList.add('hidden');
   subtitle.classList.add('hidden');
   setWord();
+  window.addEventListener('keyup', checkLetter);
 }
 
 function setWord() {
   const word = words[0];
-  const p = document.createElement('p');
-  const textNode = document.createTextNode(word);
-  p.appendChild(textNode);
-  wordContainer.appendChild(p);
+  const wordArr = word.split('');
+  wordArr.forEach(letter => {
+    const p = document.createElement('p');
+    wordContainer.appendChild(p);
+  })
 }
+
+function checkLetter() {
+  
+}
+
+startGame();
