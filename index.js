264c91d2bd36cf3dd65027a12414809419dd208f
robpwatkins <robpwatkins@gmail.com>
2021-11-15 19:07:37 -0600

    now most logic is in place, next need to account for multiple words

--- index.js
+++ index.js
@@ -2,34 +2,57 @@ import { words } from './vocab.js';
 const title = document.querySelector('#title');
 const subtitle = document.querySelector('#subtitle');
 const wordContainer = document.querySelector('#word-container');
+const guessesContainer = document.querySelector('#guessesContainer');
+const guesses = document.querySelector('#guesses');
+const gameOver = document.querySelector('#game-over');
+let wordArr;
+let correctGuessesArr = [];
+let guessesLeft = 5;
 window.addEventListener('keydown', startGame, { once: true });
 
 function startGame() {
   title.classList.add('hidden');
   subtitle.classList.add('hidden');
-  let guesses = 5;
-  const word = words[3];
-  const wordArr = word.split('');
-  setWord(wordArr);
-  const doCheckLetter = event => checkLetter(event, guesses);
-  setTimeout(() => window.addEventListener('keyup', doCheckLetter), 100);
-}
+  guessesContainer.classList.remove('hidden');
+  setWord();
+  setTimeout(() => window.addEventListener('keyup', handleKeyup), 100);
+};
 
-function setWord(wordArr) {
+function setWord() {
+  const word = words[3];
+  wordArr = word.split('');
   wordArr.forEach(letter => {
     const p = document.createElement('p');
     p.classList.add(letter);
     wordContainer.appendChild(p);
   })
-}
+};
 
-function checkLetter(event) {
+function handleKeyup(event) {
   const letter = event.key;
+  if (!wordArr.includes(letter)) return handleWrongGuess();
   const letterElArr = document.querySelectorAll(`.${letter}`);
-  if (letterElArr.length) {
-    letterElArr.forEach(el => {
+  letterElArr.forEach(el => {
+    if (!el.innerHTML) {
       const textNode = document.createTextNode(letter);
       el.appendChild(textNode);
-    })
-  }
+      correctGuessesArr.push(letter);
+    } else return console.log(`You already guessed '${letter}'!`)
+  })
+  if (correctGuessesArr.length == wordArr.length) return handleWin();
+};
+
+function handleWrongGuess() {
+  guessesLeft--;
+  guesses.innerHTML = guessesLeft;
+  if (guessesLeft == 0) return handleLose();
+}
+
+function handleWin() {
+  console.log('You win!');
+}
+
+function handleLose() {
+  guessesContainer.classList.add('hidden');
+  gameOver.classList.remove('hidden');
 }
