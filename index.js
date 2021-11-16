0e276646ebac6f85d815d2a9efa9455c8a6fd09a
robpwatkins <robpwatkins@gmail.com>
2021-11-15 23:21:38 -0600

    got it working with multiple words

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import { words } from './vocab.js';
 const title = document.querySelector('#title');
 const subtitle = document.querySelector('#subtitle');
 const wordContainer = document.querySelector('#word-container');
-const guessesContainer = document.querySelector('#guessesContainer');
+const guessesContainer = document.querySelector('#guesses-container');
 const guesses = document.querySelector('#guesses');
+const win = document.querySelector('#win');
 const gameOver = document.querySelector('#game-over');
-let wordArr;
+let lettersArr;
 let correctGuessesArr = [];
 let guessesLeft = 5;
 window.addEventListener('keydown', startGame, { once: true });
@@ -18,47 +19,50 @@ function startGame() {
   setTimeout(() => window.addEventListener('keyup', handleKeyup), 100);
 };
 
-startGame()
-
 function setWord() {
   const word = words[3];
-  wordArr = word.split('');
-  wordArr.forEach(letter => {
-    const p = document.createElement('p');
-    if (letter === ' ') {
-      letter = '_'
-      p.classList.add('space', 'text');
-    }
-    p.classList.add(letter, 'text');
-    wordContainer.appendChild(p);
+  lettersArr = word.replace(/ /g, '').split('');
+  word.split(' ').forEach(word => {
+    const div = document.createElement('div');
+    div.classList.add('word');
+    wordContainer.appendChild(div);
+    word.split('').forEach(letter => {
+      const p = document.createElement('p');
+      p.classList.add(letter, 'letter');
+      div.appendChild(p);
+    })
   })
 };
 
 function handleKeyup(event) {
   const letter = event.key;
-  if (!wordArr.includes(letter)) return handleWrongGuess();
+  if (!lettersArr.includes(letter)) return handleWrongGuess();
   const letterElArr = document.querySelectorAll(`.${letter}`);
   letterElArr.forEach(el => {
     if (!el.innerHTML) {
       const textNode = document.createTextNode(letter);
       el.appendChild(textNode);
       correctGuessesArr.push(letter);
-    } else return console.log(`You already guessed '${letter}'!`)
+    } else return console.log(`You already guessed '${letter}'!`);
   })
-  if (correctGuessesArr.length == wordArr.length) return handleWin();
+  console.log(correctGuessesArr, lettersArr);
+  if (correctGuessesArr.length == lettersArr.length) return handleWin();
 };
 
 function handleWrongGuess() {
   guessesLeft--;
   guesses.innerHTML = guessesLeft;
   if (guessesLeft == 0) return handleLose();
-}
+};
 
 function handleWin() {
-  console.log('You win!');
-}
+  guessesContainer.classList.add('hidden');
+  win.classList.remove('hidden');
+  window.removeEventListener('keyup', handleKeyup);
+};
 
 function handleLose() {
   guessesContainer.classList.add('hidden');
   gameOver.classList.remove('hidden');
-}
+  window.removeEventListener('keyup', handleKeyup);
+};
