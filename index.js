9f208cca6c4327182cf5d57685691d6e0d251485
robpwatkins <robpwatkins@gmail.com>
2021-11-24 15:59:54 -0600

    committing for later

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const guessesContainer = document.querySelector('#guesses-container');
 const incorrectGuesses = document.getElementById('incorrect-guesses');
 const incorrectLetters = document.getElementById('incorrect-letters');
 const fawkes = document.querySelector('#fawkes');
-const feast = document.querySelector('#feast');
+const fawkesChomp1 = document.getElementById('fawkes-chomp-1');
+const fawkesChomp2 = document.getElementById('fawkes-chomp-2');
+const fawkesChomp3 = document.getElementById('fawkes-chomp-3');
+const guessesAndFeast = document.getElementById('guesses-and-feast');
+const feastTop = document.querySelector('#feast-top');
+const feastBottom = document.querySelector('#feast-bottom');
 const win = document.querySelector('#win');
 const gameOver = document.querySelector('#game-over');
+let word;
 let lettersObj = {};
 let lettersArr;
 let correctGuessesArr = [];
@@ -24,8 +30,8 @@ function startGame() {
   instructions.style.transform = 'translateY(7vh)';
   fawkes.classList.remove('hidden');
   incorrectGuesses.style.display = 'flex';
-  feast.classList.remove('hidden');
-  revealInstructions();
+  feastTop.classList.remove('hidden');
+  feastBottom.classList.remove('hidden');
   setWord();
   setIncorrectGuesses();
   setTimeout(() => window.addEventListener('keyup', handleKeyup), 500);
@@ -33,11 +39,8 @@ function startGame() {
 
 // startGame();
 
-function revealInstructions() {
-}
-
 function setWord() {
-  const word = words[12];
+  word = words[12];
   lettersArr = word.replace(/ /g, '').split('');
   word.split(' ').forEach(word => {
     const div = document.createElement('div');
@@ -101,12 +104,54 @@ function handleCorrectGuess(key) {
 function handleWin() {
   guessesContainer.classList.add('hidden');
   win.classList.remove('hidden');
-  fawkes.style.transform = 'scaleX(-1);';
+  // defineWord();
   window.removeEventListener('keyup', handleKeyup);
 };
 
+// handleLose();
+
 function handleLose() {
-  guessesContainer.classList.add('hidden');
+  // guessesContainer.classList.add('hidden');
+  setTimeout(() => {
+    console.log('fawkesPosition: ', fawkesPosition);
+    fawkes.classList.add('hidden');
+    fawkesChomp1.classList.remove('hidden');
+    fawkesChomp1.style.transform = `translateX(${fawkesPosition}vw)`;
+  }, 400);
+  setTimeout(() => {
+    fawkesChomp1.classList.add('hidden');
+    fawkesChomp2.style.transform = `translateX(${fawkesPosition}vw)`;
+    fawkesChomp2.classList.remove('hidden');
+  }, 1000);
+  setTimeout(() => {
+    fawkesChomp2.classList.add('hidden');
+    fawkesChomp3.style.transform = `translateX(${fawkesPosition}vw)`;
+    fawkesChomp3.classList.remove('hidden');
+    feastTop.style.zIndex = '1';
+    feastTop.style.filter = 'none';
+    feastTop.style.transform = 'rotate(-32deg) translateY(-7vw)';
+  }, 1500);
+  setTimeout(() => feastTop.style.transform = 'rotate(-25deg) translateY(-7vw) translateX(-55vw)', 3000);
+  setTimeout(() => guessesAndFeast.style.overflow = 'hidden', 3000);
+  setTimeout(() => {
+    fawkesChomp3.classList.add('hidden');
+    fawkesChomp1.classList.remove('hidden');
+  }, 3500);
+  setTimeout(() => {
+    fawkesChomp1.classList.add('hidden');
+    fawkes.classList.remove('hidden');
+  }, 5000);
   gameOver.classList.remove('hidden');
   window.removeEventListener('keyup', handleKeyup);
 };
+
+// async function defineWord() {
+//   const word = 'cider';
+//   const options = {
+
+//   }
+//   const response = await (await fetch(`https://api.wordnik.com/word.json/${word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=${process.env.WORDNIK_API_KEY}`)).json();
+//   console.log('response: ', response);
+// };
+
+// defineWord();
